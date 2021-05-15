33ddf0c38fdf1460df15612c257a8f91804fa63c
James Duong <james.duong93@gmail.com>
2021-05-15 13:21:12 +1000

    add types and util functions

---
+++ src/types.ts
@@ -0,0 +1,31 @@
+export interface Task {
+  id: string;
+  name: string;
+  priority: Priority;
+  status: Status;
+}
+
+export enum Priority {
+  Low,
+  Normal,
+  Important,
+}
+
+export enum Status {
+  Active = "ACTIVE",
+  Completed = "COMPLETED",
+}
+
+export enum Filter {
+  All = "ALL",
+  Active = "ACTIVE",
+  Completed = "COMPLETED",
+}
+
+export enum Sort {
+  New = "new",
+  Priority = "priority",
+  Name = "name",
+}
+
+export type OrderParams = "asc" | "desc";
---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export const generateRandomId = () => Math.random().toString(36).substr(2, 5);
+
+export const validateTextInput = (input: string) => !!input.trim();
